77da6699d8f55e899ae79dc9e637949bf4c1f256
localMax <44784974+localMax@users.noreply.github.com>
2019-02-28 18:54:00 -0600

    Update Graph.java

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -1,5 +1,29 @@
 package graph;
 
-public class Graph {
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
+public class Graph {
+	final Map<String,LinkedList<Rail>> cityMap = new HashMap<>();//Trying a map sortta adjacency list maybe TreeMap(prob not)?
+	
+	public void add(String cityName, Rail rail) {
+		LinkedList<Rail> rails;
+		if(!cityMap.containsKey(cityName)) {
+			 rails = new LinkedList<Rail>();
+			rails.add(rail);
+			cityMap.put(cityName, rails);
+		}else {
+			rails = cityMap.get(cityName);
+			if(!rails.contains(rail))rails.add(rail);
+			cityMap.put(cityName,  rails);
+		}
+	}
+	public void add(String cityName, LinkedList<Rail> rails) {
+		if(!cityMap.containsKey(cityName)) {
+			cityMap.put(cityName, rails);
+		}else {
+			//i want it to throw something because this should never happen
+		}
+	}
 }
