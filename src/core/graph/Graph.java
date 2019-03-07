4b9d19462f4a93a3da694495e01a4585cdc40689
localMax <44784974+localMax@users.noreply.github.com>
2019-03-07 16:15:19 -0600

    longest Path

--- src/core/graph/Graph.java
+++ src/core/graph/Graph.java
@@ -16,14 +16,7 @@ public class Graph {
 	final Map<String, LinkedList<Rail>> cityMap = new HashMap<>();// Trying a map sortta adjacency list maybe
 																	// TreeMap(prob not)?
 
-	public Graph(File fileName) throws FileNotFoundException {
-		Scanner sc = new Scanner(fileName);
-		while (sc.hasNextLine()) {
-			String[] input = sc.nextLine().split(" ");
-			add(input[0],
-					new Rail(input[0], input[1], Integer.parseInt(input[2]), Boolean.parseBoolean(input[3]), input[4]));
-		}
-		// System.out.println(cityMap);
+	public Graph(){
 	}
 
 	// want to make this recursive
