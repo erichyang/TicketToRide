37d914791fa2ae0b3012e47afaecf9f8af75fdcd
localMax <44784974+localMax@users.noreply.github.com>
2019-03-26 13:00:51 -0500

    made Ticket class

--- src/core/Ticket.java
+++ src/core/Ticket.java
@@ -2,4 +2,28 @@ package core;
 
 public class Ticket {
 
+	private String city1;
+	private String city2;
+	private int points;
+	
+	public Ticket(String ticketsLine){
+		String[] lineData = ticketsLine.split("|");
+		points = Integer.parseInt(lineData[0]);
+		city1 = lineData[1];
+		city2 = lineData[2];
+	}
+	
+	public Ticket(int pointNum,String cityOne,String cityTwo) {
+		points = pointNum;
+		city1 = cityOne;
+		city2 = cityTwo;
+	}
+	
+	public String getCities() {
+		return city1+","+city2;
+	}
+	
+	public int getPointCount() {
+		return points;
+	}
 }
