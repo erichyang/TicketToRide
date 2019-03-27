3aa80f01807b461047d567b8c2a85848c0e771d1
localMax <44784974+localMax@users.noreply.github.com>
2019-03-27 13:40:23 -0500

    Start Gm

--- src/core/Player.java
+++ src/core/Player.java
@@ -1,5 +1,97 @@
 package core;
 
-public class Player {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import graph.Rail;
+
+public class Player {
+	
+	private GameEventListener listen;
+	private int trains;
+	private int points;
+	private ArrayList<String> hand;
+	private ArrayList<Ticket> tickets;
+	private String name;
+	private ArrayList<Set<String>> cities;
+	
+	public Player(String playerName,ArrayList<String> trainCards, ArrayList<Ticket> chosenTickets) {
+		trains = 45;
+		points = 0;
+		hand = null;
+		tickets = null;
+		name = playerName;
+	}
+
+	public void setListener(GameEventListener GEL) {
+		listen = GEL;
+	}
+	
+	public ArrayList<String> getHand() {
+		return hand;
+	}
+
+	public void setHand(ArrayList<String> hand) {
+		this.hand = hand;
+	}
+
+	public ArrayList<Ticket> getTickets() {
+		return tickets;
+	}
+
+	public void setTickets(ArrayList<Ticket> tickets) {
+		this.tickets = tickets;
+	}
+	
+	public void addRail(Rail rail) {
+		String cityA = rail.getCityA();
+		String cityB = rail.getCityB();
+		
+		int aLocation = findCity(cityA);
+		int bLocation = findCity(cityB);
+		
+		if(aLocation == -1 && bLocation == -1) 
+			cities.add(Stream.of(cityA, cityB).collect(Collectors.toSet()));
+		else if(bLocation == -1)
+			cities.get(aLocation).add(cityB);
+		else if(aLocation == -1)
+			cities.get(bLocation).add(cityA);
+		else {
+			Set<String> aGroup = cities.get(aLocation);
+			Set<String> bGroup = cities.get(bLocation);
+			Set<String> mergeGroup = new HashSet<String>(aGroup);
+			mergeGroup.addAll(bGroup);
+			
+			cities.remove(aLocation);
+			cities.remove(bLocation);
+			
+			cities.add(mergeGroup);
+		}
+	}
+	
+	private int findCity(String city) {
+		for(int i=0; i< cities.size();i++) {
+			if(cities.get(i).contains(city)) return i;
+		}
+		return -1;
+	}
+	
+	public void addPoints(int value) {
+		points += value;
+	}
+	
+	public void addCard(String newTrainCard) {
+		hand.add(newTrainCard);
+	}
+	
+	public void addTicket(Ticket newTicket) {
+		tickets.add(newTicket);
+	}
+	
+	public String getName() {
+		return name;
+	}
 }
--- src/core/TicketToRide.java
+++ src/core/TicketToRide.java
@@ -1,5 +1,37 @@
 package core;
 
-public class TicketToRide {
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
+import graph.Graph;
+
+public class TicketToRide implements GameEventListener,PlayerEventListener{
+
+	private static final int[] pointValues = {0,1,2,4,7,15,21};
+	
+	private Graph graph;
+	private Queue<Player> players;
+	private int roundWeight;
+	private deck GameDeck;
+	private Stack<Ticket> tickets;
+	private String[] visibleCards;
+	
+	public TicketToRide(){
+		
+		players = new LinkedList<Player>();
+		players.add(new Player("Cecil Rhails",new ArrayList<String>(),new ArrayList<Ticket>()));
+		players.add(new Player("John Henry",new ArrayList<String>(),new ArrayList<Ticket>()));
+		players.add(new Player("Henry Stanley",new ArrayList<String>(),new ArrayList<Ticket>()));
+		players.add(new Player("John Rockefeller",new ArrayList<String>(),new ArrayList<Ticket>()));
+		players.forEach(player -> player.setListener(this));
+		
+		roundWeight =0;
+		
+		GameDeck = new deck();
+		
+		tickets = new Stack<Ticket>();
+		tickets 
+	}
 }
