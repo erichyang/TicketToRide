2b1b8a8443705c92753a5631e4c3b478ad10ee75
Eric Yang <43301578+erichyang@users.noreply.github.com>
2019-04-01 13:01:16 -0500

    resolve conflict

--- src/core/Player.java
+++ src/core/Player.java
@@ -1,113 +1,126 @@
 package core;
 
 import java.util.ArrayList;
-import java.util.Set;
-
-import graph.Rail;
-
-public class Player {
-	
-=======
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import graph.Rail;
+import core.graph.Rail;
 
-public class Player {
-	
+public class Player
+{
+
 	private GameEventListener listen;
 	private int trains;
 	private int points;
-	private ArrayList<String> hand;
+	private HashMap<String, int> hand;
 	private ArrayList<Ticket> tickets;
 	private String name;
 	private ArrayList<Set<String>> cities;
-	
-	public Player(String playerName,ArrayList<String> trainCards, ArrayList<Ticket> chosenTickets) {
+
+	public Player(String playerName, ArrayList<String> trainCards, ArrayList<Ticket> chosenTickets)
+	{
 		trains = 45;
 		points = 0;
-		hand = null;
+		hand = new HashMap<>();
 		tickets = null;
 		name = playerName;
 	}
 
-	public void setListener(GameEventListener GEL) {
+	public void setListener(GameEventListener GEL)
+	{
 		listen = GEL;
 	}
-	
-	public ArrayList<String> getHand() {
+
+	public ArrayList<String> getHand()
+	{
 		return hand;
 	}
 
-	public void setHand(ArrayList<String> hand) {
+	public void setHand(ArrayList<String> hand)
+	{
 		this.hand = hand;
 	}
 
-	public ArrayList<Ticket> getTickets() {
+	public ArrayList<Ticket> getTickets()
+	{
 		return tickets;
 	}
 
-	public void setTickets(ArrayList<Ticket> tickets) {
+	public void setTickets(ArrayList<Ticket> tickets)
+	{
 		this.tickets = tickets;
 	}
-	
-	public void addRail(Rail rail) {
-		
+
+	public void addRail(Rail rail)
+	{
+
 		String cityA = rail.getCityA();
 		String cityB = rail.getCityB();
-		
+
 		int aLocation = findCity(cityA);
 		int bLocation = findCity(cityB);
-		
-		if(aLocation == -1 && bLocation == -1) 
+
+		if (aLocation == -1 && bLocation == -1)
 			cities.add(Stream.of(cityA, cityB).collect(Collectors.toSet()));
-		else if(bLocation == -1)
+		else if (bLocation == -1)
 			cities.get(aLocation).add(cityB);
-		else if(aLocation == -1)
+		else if (aLocation == -1)
 			cities.get(bLocation).add(cityA);
-		else {
+		else
+		{
 			Set<String> aGroup = cities.get(aLocation);
 			Set<String> bGroup = cities.get(bLocation);
 			Set<String> mergeGroup = new HashSet<String>(aGroup);
 			mergeGroup.addAll(bGroup);
-			
+
 			cities.remove(aLocation);
 			cities.remove(bLocation);
 			cities.add(mergeGroup);
 		}
 	}
-	
-	private int findCity(String city) {
-		
-		for(int i=0; i< cities.size();i++) {
-			if(cities.get(i).contains(city)) return i;
+
+	private int findCity(String city)
+	{
+
+		for (int i = 0; i < cities.size(); i++)
+		{
+			if (cities.get(i).contains(city))
+				return i;
 		}
 		return -1;
 	}
-	
-	public void addPoints(int value) {
+
+	public void addPoints(int value)
+	{
 		points += value;
 	}
-	
-	public void addCard(String newTrainCard) {
+
+	public void addCard(String newTrainCard)
+	{
 		hand.add(newTrainCard);
 	}
-	
-	public void addTicket(Ticket newTicket) {
+
+	public void addTicket(Ticket newTicket)
+	{
 		tickets.add(newTicket);
 	}
-	
-	public void removeTicket(String ticketName) {
-		for(int i=tickets.size()-1; i>=0; i--) {
-			if(tickets.get(i).getCities().equals(ticketName)) {
+
+	public void removeTicket(String ticketName)
+	{
+		for (int i = tickets.size() - 1; i >= 0; i--)
+		{
+			if (tickets.get(i).getCities().equals(ticketName))
+			{
 				tickets.remove(i);
 			}
 		}
 	}
-	
-	public String getName() {
+
+	public String getName()
+	{
 		return name;
 	}
 }
--- src/core/TicketToRide.java
+++ src/core/TicketToRide.java
@@ -7,109 +7,125 @@ import java.util.Queue;
 import java.util.Scanner;
 import java.util.Stack;
 
-import graph.Graph;
+import core.graph.Graph;
 
-public class TicketToRide implements GameEventListener,PlayerEventListener{
+public class TicketToRide implements GameEventListener, PlayerEventListener
+{
 
-	private static final int[] pointValues = {0,1,2,4,7,15,21};
-	
+	private static final int[] pointValues =
+	{ 0, 1, 2, 4, 7, 15, 21 };
+
 	private Graph graph;
 	private Queue<Player> players;
 	private int roundWeight;
 	private Deck GameDeck;
 	private Stack<Ticket> tickets;
 	private String[] visibleCards;
-	
-	public TicketToRide(){
-		
+
+	public TicketToRide()
+	{
+
 		players = new LinkedList<Player>();
-		players.add(new Player("Rhail island Z",new ArrayList<String>(),new ArrayList<Ticket>()));
-		players.add(new Player("Cleveland Z",new ArrayList<String>(),new ArrayList<Ticket>()));
-		players.add(new Player("Smashboy",new ArrayList<String>(),new ArrayList<Ticket>()));
-		players.add(new Player("Teewee",new ArrayList<String>(),new ArrayList<Ticket>()));
+		players.add(new Player("Rhail island Z", new ArrayList<String>(), new ArrayList<Ticket>()));
+		players.add(new Player("Cleveland Z", new ArrayList<String>(), new ArrayList<Ticket>()));
+		players.add(new Player("Smashboy", new ArrayList<String>(), new ArrayList<Ticket>()));
+		players.add(new Player("Teewee", new ArrayList<String>(), new ArrayList<Ticket>()));
 		players.forEach(player -> player.setListener(this));
-		
-		roundWeight =0;
-		
+
+		roundWeight = 0;
+
 		GameDeck = new Deck();
-		
+
 		tickets = new Stack<Ticket>();
 		Scanner sc = new Scanner(new File("game_files//tickets.in"));
-		
-		while(sc.hasNextLine())
+
+		while (sc.hasNextLine())
 			tickets.add(new Ticket(sc.nextLine()));
-		
+
 		visibleCards = new String[5];
 	}
-	
-	public void onPlayerEvent(PlayerEvent e) {
-		
-		if((roundWeight+e.getWeight()) > 2) return; 
-			
+
+	public void onPlayerEvent(PlayerEvent e)
+	{
+
+		if ((roundWeight + e.getWeight()) > 2)
+			return;
+
 		int eventID = e.getID();
 		Player currentPlayer = players.peek();
-		
-		if(eventID <= 4 && eventID >= 0) {
+
+		if (eventID <= 4 && eventID >= 0)
+		{
 			int index = eventID;
 			currentPlayer.addCard(visibleCards[index]);
 			visibleCards[index] = GameDeck.getCard();
-		}
-		else if(eventID == 5) {
+		} else if (eventID == 5)
+		{
 			currentPlayer.addCard(GameDeck.getCard());
-		}
-		else if(eventID == 6) {
+		} else if (eventID == 6)
+		{
 			currentPlayer.addTicket(tickets.pop());
-		}
-		else if(eventID == 7) {
-			//currentPlayer.removeTicket(eventID);
-			//IDK how to do this yet Ill wait for eric
-		}
-		else if(eventID <= Graph.keySet().size()+7) {
-			//hashtable stuff neeeded
-			Player source = (Player)e.getSource();
+		} else if (eventID == 7)
+		{
+			// currentPlayer.removeTicket(eventID);
+			// IDK how to do this yet Ill wait for eric
+		} else if (eventID <= Graph.keySet().size() + 7)
+		{
+			// hashtable stuff neeeded
+			Player source = (Player) e.getSource();
 			source.addRail();
-		}
-		else throw new IllegalArgumentException("invalid GameEvent ID number");
-		
+		} else
+			throw new IllegalArgumentException("invalid GameEvent ID number");
+
 		roundWeight += e.getWeight();
-		
-		if(roundWeight == 2) {
+
+		if (roundWeight == 2)
+		{
 			nextRound();
 		}
-		
+
 	}
-	
-	public void onGameEvent(GameEvent e) {
+
+	public void onGameEvent(GameEvent e)
+	{
 		int eventID = e.getID();
-		
-		if(eventID == 0) {//wating on eric
+
+		if (eventID == 0)
+		{// wating on eric
 			endGame(players.peek());
 		}
-		if(eventID == 1) {
-			if (e.getSource() instanceof Deck) {
+		if (eventID == 1)
+		{
+			if (e.getSource() instanceof Deck)
+			{
 				e.getSource().refillDeck();
-			}
-			else throw new IllegalArgumentException("Not Deck");
+			} else
+				throw new IllegalArgumentException("Not Deck");
 		}
-		if(eventID == 2) {
-			
-		}
-		else throw new IllegalArgumentException("invalid PlayerEvent ID number");
+		if (eventID == 2)
+		{
+
+		} else
+			throw new IllegalArgumentException("invalid PlayerEvent ID number");
 	}
-	
-	public void nextRound() {
+
+	public void nextRound()
+	{
 		players.offer(players.poll());
 	}
-	
-	public Graph getGraph() {
+
+	public Graph getGraph()
+	{
 		return graph;
 	}
-	
-	public Player endGame(Player current) {
-		
+
+	public Player endGame(Player current)
+	{
+
 	}
-	
-	public Player getCurrentPlayer() {
+
+	public Player getCurrentPlayer()
+	{
 		return players.peek();
 	}
 }
