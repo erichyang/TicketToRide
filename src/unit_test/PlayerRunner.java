6ebe153343870bd00d3f14992610001a950d629c
Andores <luengoad@gmail.com>
2019-04-04 13:22:32 -0500

    Finished player testing

--- src/unit_test/PlayerRunner.java
+++ src/unit_test/PlayerRunner.java
@@ -13,11 +13,11 @@ public class PlayerRunner
 	public static void main(String[] args) throws FileNotFoundException
 	{
 		Scanner input = new Scanner(System.in);
-		System.out.println("1 - Ticket, 2 - Train, 3 - Points, 4 - Rails, 5 - ");
+		System.out.println("1 - Ticket, 2 - Train, 3 - Points, 4 - Rails, 5 - Use Cards");
 		Player player = new Player("Gamer", new ArrayList<String>(), new ArrayList<Ticket>());
 		do
 		{
-			
+
 			int choice = input.nextInt();
 			input.nextLine();
 			switch (choice)
@@ -40,18 +40,25 @@ public class PlayerRunner
 			case 4:
 				System.out.print("Input the rails: ");
 				String[] railData = input.nextLine().split("\\|");
-				player.addRail(new Rail(railData[0],railData[1],Integer.parseInt(railData[2]),Boolean.parseBoolean(railData[3]),railData[4]));
+				player.addRail(new Rail(railData[0], railData[1], Integer.parseInt(railData[2]),
+						Boolean.parseBoolean(railData[3]), railData[4]));
+				break;
+			case 5:
+				System.out.print("Choose cards to remove: ");
+				String[] cardData = input.nextLine().split("\\|");
+				player.useCards(cardData[0], Integer.parseInt(cardData[1]));
 				break;
 			}
 			printPlayerHand(player);
-			System.out.println("1 - Ticket, 2 - Train, 3 - Points, 4 - Rails");
-		}while (input.hasNext());
+			System.out.println("1 - Ticket, 2 - Train, 3 - Points, 4 - Rails, 5 - Use Cards");
+		} while (input.hasNext());
 	}
-	
+
 	private static void printPlayerHand(Player pl)
 	{
-		
-		System.out.println("Cards: " + pl.getHand().toString() + "\nTickets: " + pl.getTickets().toString() + "\nPoints: " + pl.getPoints() + "\nCities: " + pl.getCities());
+
+		System.out.println("Cards: " + pl.getHand().toString() + "\nTickets: " + pl.getTickets().toString()
+				+ "\nPoints: " + pl.getPoints() + "\nCities: " + pl.getCities());
 	}
 
 }
---
+++ src/unit_test/TextRunner.java
@@ -0,0 +1,25 @@
+package unit_test;
+
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import core.Player;
+import core.TicketToRide;
+
+public class TextRunner
+{
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		TicketToRide game = new TicketToRide();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Current Player: " + game.getCurrentPlayer().getName());
+		printPlayerHand(game.getCurrentPlayer());
+	}
+	
+	private static void printPlayerHand(Player pl)
+	{
+
+		System.out.println("Cards: " + pl.getHand().toString() + "\nTickets: " + pl.getTickets().toString()
+				+ "\nPoints: " + pl.getPoints() + "\nCities: " + pl.getCities());
+	}
+}
