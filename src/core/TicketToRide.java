326eec780cb96d35de753ef42deecc854ac2b757
localMax <44784974+localMax@users.noreply.github.com>
2019-04-23 12:52:41 -0500

    Update TicketToRide.java

--- src/core/TicketToRide.java
+++ src/core/TicketToRide.java
@@ -227,7 +227,7 @@ public class TicketToRide implements GameEventListener, PlayerEventListener
 				winner = p;
 			}
 		}
-		observer.observe(new ViewEvent(1, this, players, GameDeck, graph));
+		observer.observe(new ViewEvent(1, this, players, GameDeck, graph,visibleCards));
 		return winner;
 	}
 
