878d61ad8252f7f718c26c2f7a7424d5fc70646e
localMax <44784974+localMax@users.noreply.github.com>
2019-05-05 12:44:49 -0500

    housekeeping

--- src/core/TicketToRide.java
+++ src/core/TicketToRide.java
@@ -8,14 +8,12 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 import java.util.Stack;
-
 import core.graph.Graph;
 import core.graph.Rail;
 import graphics.View;
 import graphics.ViewEvent;
 
-public class TicketToRide implements GameEventListener, PlayerEventListener
-{
+public class TicketToRide implements GameEventListener, PlayerEventListener {
 
 	private View observer;
 	private Graph graph;
@@ -24,9 +22,9 @@ public class TicketToRide implements GameEventListener, PlayerEventListener
 	private Deck GameDeck;
 	private Stack<Ticket> tickets;
 	private String[] visibleCards;
+	private final int[] pointValues = { 1, 2, 4, 7, 10, 15 };
 
-	public TicketToRide() throws FileNotFoundException
-	{
+	public TicketToRide() throws FileNotFoundException {
 		GameDeck = new Deck();
 		GameDeck.setListener(this);
 
@@ -36,8 +34,7 @@ public class TicketToRide implements GameEventListener, PlayerEventListener
 		players.add(new Player("Smashboy", new ArrayList<String>(), new ArrayList<Ticket>()));
 		players.add(new Player("Teewee", new ArrayList<String>(), new ArrayList<Ticket>()));
 
-		players.forEach(player ->
-		{
+		players.forEach(player -> {
 			player.setListener(this);
 			for (int i = 0; i < 4; i++)
 				player.addCards(GameDeck.getCard(false));
@@ -56,120 +53,99 @@ public class TicketToRide implements GameEventListener, PlayerEventListener
 
 		graph = new Graph();
 		sc = new Scanner(new File("game_files\\cities\\graph.in"));
-		while (sc.hasNextLine())
-		{
+		while (sc.hasNextLine()) {
 			int num = sc.nextInt();
 			sc.nextLine();
-			for (int i = 0; i < num; i++)
-			{
+			for (int i = 0; i < num; i++) {
 				String[] input = sc.nextLine().split(" ");
 				graph.add(input[0], new Rail(input[0], input[1], Integer.parseInt(input[2]),
 						Boolean.parseBoolean(input[3]), (input.length == 5) ? input[4] : input[4] + ";" + input[5]));
 			}
 		}
-		// System.out.println(graph.indexList());
 		sc.close();
 	}
 
-	public void setView(View observe)
-	{
+	public void setView(View observe) {
 		observer = observe;
-		observer.observe(new ViewEvent(2, this, players, GameDeck, graph, visibleCards, tickets,roundWeight));
+		observer.observe(new ViewEvent(2, this, players, GameDeck, graph, visibleCards, tickets, roundWeight));
 		checkVis();
 	}
 
-	public void onPlayerEvent(PlayerEvent e)
-	{
-		// System.out.println("ID "+e.getID());
-		if ((roundWeight + e.getWeight()) > 2) return;
+	public void onPlayerEvent(PlayerEvent e) {
+		if ((roundWeight + e.getWeight()) > 2)
+			return;
 
 		int eventID = e.getID();
 		Player currentPlayer = players.peek();
 
-		if (eventID == -1)
-		{
+		if (eventID == -1) {
 			nextRound();
-			//System.out.println("Forced Next Turn");
 			return;
-		} else if (eventID <= 4 && eventID >= 0)
-		{
-			// System.out.println("RoundWeight: "+roundWeight);
+			
+		} else if (eventID <= 4 && eventID >= 0) {
 			int index = eventID;
-			String card = visibleCards[index];
-//			System.out.println("Card:"+ card);			
-			if (card == "") return;
-			if (card == "Wild")
-			{
-				if (roundWeight > 0)
-				{
+			String card = visibleCards[index];	
+			if (card == "")
+				return;
+			if (card == "Wild") {
+				if (roundWeight > 0) {
 					this.onPlayerEvent(e.reEvent());
 					return;
 				}
 				roundWeight++;
 			}
-
 			visibleCards[index] = GameDeck.getCard(false);
 			checkVis();
-			if (currentPlayer.addCards(card) == null){
+			if (currentPlayer.addCards(card) == null)
+				return;			
+		} else if (eventID == 5) {
+			if (currentPlayer.addCards(GameDeck.getCard(false)) == null)
 				return;
-			}
-		} else if (eventID == 5)
-		{
-			if (currentPlayer.addCards(GameDeck.getCard(false)) == null) return;
-		} else if (eventID == 6)
-		{
+		} else if (eventID == 6) {
 			getCurrentPlayer().addTicket(tickets.pop());
-		} else if (eventID == 7)
-		{
+		} else if (eventID == 7) {
 			Stack<Ticket> temp = new Stack<Ticket>();
 			temp.add(tickets.pop());
 			temp.addAll(tickets);
 			tickets = temp;
-			// eventID is rail number * 10 + 8 if number ends in 9, is a single rail or the
-			// first rail of the double rail.
-			// If it is 0, then it is the second rail of a double rail
+			/* eventID is rail number * 10 + 8 if number ends in 9, is a single rail or the
+			first rail of the double rail.
+			If it is 0, then it is the second rail of a double rail*/
 		} else if (eventID <= 10 * (graph.indexList().size() - 1) + 8 && eventID >= 8
-				&& (eventID % 10 == 8 || eventID % 10 == 9))
-		{
+				&& (eventID % 10 == 8 || eventID % 10 == 9)) {
 			Player current = getCurrentPlayer();
 			Rail rail = graph.getRail((eventID - 8) / 10);
 			Rail inverse = graph.getInverse(rail);
 			String origColor = rail.getColor();
-			int railNum =0;
+			int railNum = 0;
 
-			if (eventID % 10 == 8)
-			{
+			if (eventID % 10 == 8) {
 				railNum = 0;
 				rail.setColor(rail.getColor().split(";")[0]);
-			} else if (eventID % 10 == 9)
-			{
-				// System.out.println(rail.getColor());
+			} else if (eventID % 10 == 9) {
 				railNum = 1;
 				rail.setColor(rail.getColor().split(";")[1]);
 			} else
 				throw new IllegalArgumentException("invalid GameEvent ID number");
-
 			// System.out.println(rail.toString());
-
-			if (rail.getColor().equals("Gray"))
-			{
-				//System.out.println("gray rail " + rail);
+			if (rail.getColor().equals("Gray")) {
+				// System.out.println("gray rail " + rail);
 				String color = observer.color(rail.getLength());
-				//System.out.println("Color:" + color);
-				if(color==null||color.equals("")) return;
+				// System.out.println("Color:" + color);
+				if (color == null || color.equals(""))
+					return;
 				rail.setColor(color);
 			}
 			// System.out.println("Rail: "+ rail + " OrigColor: "+ origColor);
 			ArrayList<String> usedCards = current.useCards(rail);
 
-			if (usedCards == null)
-			{
-				//System.out.println("not enough cards");
-				if(origColor.split(";")[railNum].equals("Gray")) {
-					if(railNum == 0) 
-						rail.setColor(rail.getColor()+";"+origColor.split(";")[1]);
-					else if(railNum == 1)
-						rail.setColor(origColor.split(";")[1]+";"+rail.getColor());
+			if (usedCards == null) {
+				// System.out.println("not enough cards");
+				if (origColor.split(";")[railNum].equals("Gray")) {
+					if (railNum == 0)
+						rail.setColor(rail.getColor() + ";" + origColor.split(";")[1]);
+					else if (railNum == 1)
+						rail.setColor(origColor.split(";")[1] + ";" + rail.getColor());
 				}
 				rail.setColor(origColor);
 				return;
@@ -177,10 +153,8 @@ public class TicketToRide implements GameEventListener, PlayerEventListener
 
 			if (origColor.split(";")[railNum].equals(rail.getColor()) && rail.getOwnerName(railNum) == (null))
 				rail.setOwner(getCurrentPlayer().getName(), railNum);
-
-			else
-			{
-				//System.out.println("Already Owned");
+			else {
+				// System.out.println("Already Owned");
 				rail.setColor(origColor);
 				return;
 			}
@@ -188,176 +162,148 @@ public class TicketToRide implements GameEventListener, PlayerEventListener
 			inverse.setColor(rail.getColor());
 			inverse.setOwner(rail.getOwnerName(0), 0);
 			inverse.setOwner(rail.getOwnerName(railNum), railNum);
-			
+
 			usedCards.forEach(train -> GameDeck.addDiscardedCard(train));
 			checkVis();
 			current.addRail(rail);
-			final int[] pointValues =
-			{1, 2, 4, 7, 10, 15 };
-			
-			current.addPoints(pointValues[rail.getLength()-1]);
-			//System.out.println("Rail: " + rail + " OrigColor: " + origColor);
-		} else if (eventID % 10 == 6 || eventID % 10 == 7)
-		{
+
+			current.addPoints(pointValues[rail.getLength() - 1]);
+			// System.out.println("Rail: " + rail + " OrigColor: " + origColor);
+		} else if (eventID % 10 == 6 || eventID % 10 == 7) {
 			int num = eventID;
-			while (num > 1)
-			{
-				if (num % 10 == 6)
-				{
+			while (num > 1) {
+				if (num % 10 == 6) {
 					getCurrentPlayer().addTicket(tickets.pop());
-				} else
-				{
+				} else {
 					Stack<Ticket> temp = new Stack<Ticket>();
 					temp.add(tickets.pop());
 					temp.addAll(tickets);
 					tickets = temp;
 				}
-				num = num / 10;
+				num /= 10;
 			}
 		} else
 			throw new IllegalArgumentException("invalid PlayerEvent ID number");
-
 		roundWeight += e.getWeight();
 		if (roundWeight == 2)
 			nextRound();
-		observer.observe(new ViewEvent(0, this, players, GameDeck, graph, visibleCards, tickets,roundWeight));
+		observer.observe(new ViewEvent(0, this, players, GameDeck, graph, visibleCards, tickets, roundWeight));
 	}
 
-	private void checkVis()
-	{
+	private void checkVis() {
 		int count = 0;
-		for(int i=0; i< visibleCards.length; i++) {
+		for (int i = 0; i < visibleCards.length; i++) {
 			String card = visibleCards[i];
 			if (card.equals("Wild"))
 				count++;
 		}
-		int nullCount =0;
-		for(int i=0; i< visibleCards.length; i++) {
+		int nullCount = 0;
+		for (int i = 0; i < visibleCards.length; i++) {
 			String card = visibleCards[i];
-			if(card.equals("")) {
-			nullCount++;	
-			visibleCards[i] = GameDeck.getCard(count >= 2);
-			if(visibleCards[i].equals("Wild")) count++;
+			if (card.equals("")) {
+				nullCount++;
+				visibleCards[i] = GameDeck.getCard(count >= 2);
+				if (visibleCards[i].equals("Wild"))
+					count++;
 			}
 		}
-		if(nullCount == 5 && roundWeight == 1) {
+		if (nullCount == 5 && roundWeight == 1)
 			this.onPlayerEvent(new PlayerEvent(-1));
-		}
 		if (count >= 3)
-		{
 			this.onGameEvent(new GameEvent(2, this));
-		}
 	}
 
-	public void onGameEvent(GameEvent e)
-	{
+	public void onGameEvent(GameEvent e) {
 		int eventID = e.getID();
 
-		if (eventID == 0)
-		{
+		if (eventID == 0) {
 			players.peek().finalTurn();
-		} else if (eventID == 1)
-		{
-			if (e.getSource() instanceof Deck)
-			{
+		} else if (eventID == 1) {
+			if (e.getSource() instanceof Deck) {
 				((Deck) e.getSource()).refillDeck();
 			} else
 				throw new IllegalArgumentException("Not Deck");
-		} else if (eventID == 2)
-		{
-			for (int i = 0; i < visibleCards.length; i++)
-			{
+		} else if (eventID == 2) {
+			for (int i = 0; i < visibleCards.length; i++) {
 				GameDeck.addDiscardedCard(visibleCards[i]);
 				visibleCards[i] = GameDeck.getCard(true);
 			}
-			//checkVis();
-		} else if (eventID == 3)
-		{
-//			endGame();
-			System.out.println(endGame());
+			// checkVis();
+		} else if (eventID == 3) {
+			endGame();
+			//System.out.println(endGame());
 		} else
 			throw new IllegalArgumentException("invalid PlayerEvent ID number");
 
-		observer.observe(new ViewEvent(3, this, players, GameDeck, graph, visibleCards, tickets,roundWeight));
+		observer.observe(new ViewEvent(3, this, players, GameDeck, graph, visibleCards, tickets, roundWeight));
 	}
 
-	public void nextRound()
-	{
+	public void nextRound() {
 		roundWeight = 0;
 		players.offer(players.poll());
-		observer.observe(new ViewEvent(0, this, players, GameDeck, graph, visibleCards, tickets,roundWeight));
+		observer.observe(new ViewEvent(0, this, players, GameDeck, graph, visibleCards, tickets, roundWeight));
 	}
 
-	public Graph getGraph()
-	{
-		return graph;
-	}
+//	public Graph getGraph() {
+//		return graph;
+//	}
 
-	public String[] getVisCards()
-	{
+	public String[] getVisCards() {
 		return visibleCards;
 	}
 
-	public Player endGame()
-	{
+	public Player endGame() {
 		onPlayerEvent(new PlayerEvent(-1));
 		for (int i = 0; i < visibleCards.length; i++)
-		{
 			visibleCards[i] = null;
-		}
 
 		int mostTickets = Integer.MIN_VALUE;
 		int longestPath = Integer.MIN_VALUE;
-//		players.forEach(player -> player.countTickets());
 		int[] paths = new int[players.size()];
 		int[] ticketNum = new int[players.size()];
-		int index =0;
-		for(Player P: players) {
+		int index = 0;
+
+		for (Player P : players) {
 			int num = P.countTickets();
 			int path = graph.LongestPath(P);
-			System.out.println(P + " , "+ path);
-			
+
 			paths[index] = path;
 			ticketNum[index] = num;
-			
-			if(mostTickets< num)
+
+			if (mostTickets < num)
 				mostTickets = num;
 
-			if(longestPath<path)
+			if (longestPath < path)
 				longestPath = path;
 
 			index++;
 		}
-		index =0;
-		for(Player P: players) {
-			if(P.getComp() == mostTickets)
+		index = 0;
+		for (Player P : players) {
+			if (P.getComp() == mostTickets)
 				P.addPoints(10);
-			if(paths[index] ==  longestPath)
-//				System.out.println(P + " , "+ paths[index]);
+			if (paths[index] == longestPath)
 				P.addPoints(15);
 			index++;
 		}
 
 		Player winner = null;
 		int mostPoints = Integer.MIN_VALUE;
-		for (Player p : players)
-		{
-			if (p.points() > mostPoints){
+		for (Player p : players) {
+			if (p.points() > mostPoints) {
 				mostPoints = p.points();
 				winner = p;
 			}
 		}
-		observer.observe(new ViewEvent(1, this, players, GameDeck, graph, visibleCards, tickets,roundWeight));
+		observer.observe(new ViewEvent(1, this, players, GameDeck, graph, visibleCards, tickets, roundWeight));
 		return winner;
 	}
 
-	public Player getCurrentPlayer()
-	{
+	public Player getCurrentPlayer() {
 		return players.peek();
 	}
-
-	public Deck getDeck()
-	{
-		return GameDeck;
-	}
+//
+//	public Deck getDeck() {
+//		return GameDeck;
+//	}
 }
